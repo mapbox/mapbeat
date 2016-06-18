713ec55d6df2784e1bbe16368dfee31d373520e9
Sajjad Anwar <me@sajjad.in>
2016-06-18 12:27:08 +0530

    use sendFile

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get('/', function (req, res) {
-  res.sendfile('index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/', function (req, res) {
